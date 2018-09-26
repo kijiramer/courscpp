51af63a7a9265666109be63400cc34ab28034c0a
kijiramer <kijiramer@hotmail.fr>
2018-09-26 12:55:51 +0200

    cours 2 pas de com sur le site

--- src/hello.h
+++ src/hello.h
@@ -1,9 +1,23 @@
+/*! \file hello.h
+* \brief description
+* \author Mehdi
+* \version 1.0
+*/
+
 #pragma once
+
+/*!
+* \class hey
+* \brief ... 
+* \
+	Ce qu'on veut decrire etc...
+* \
+*/	
 
 class hey{
 
 public:
-		void hello();
-		void hola();
-		void salut();
+		void hello();/*! \brief affiche "Hello world" */
+		void hola();/*! \brief affiche "Hola como esta" */
+		void salut();/*! \brief affiche "Coucou toi" */
 };
--- src/lib.h
+++ src/lib.h
@@ -1,12 +1,27 @@
+/*! \file lib.h
+* \brief description
+* \author Mehdi
+* \version 1.0
+*/
+
 #pragma once
+
+/*!
+* \class calcul
+* \brief additionne ou multiplie deux nombres
+* \
+	Ce qu'on veut decrire etc...
+* \
+*/	
+
 class calcul
 {
 public:
     
 
-        int somme(int nombre_1, int nombre_2);
+        int somme(int nombre_1, int nombre_2);/*! \brief Additionne deux nombres; param nombre_1 un nombre réel; param nombre_2 un nombre réel; return nombre_1+nombre_2 */
 
-        int mult(int nombre_3, int nombre_4);
+        int mult(int nombre_3, int nombre_4);/*! \brief Multiplie deux nombres; param nombre_3 un nombre réel; param nombre_4 un nombre réel; return nombre_3*nombre_4 */
 
 
 };
